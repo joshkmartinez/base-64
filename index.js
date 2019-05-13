e8288e8724e65409b280f7e1e4ffcae386fa7993
Joshua Martinez <joshkmartinez@gmail.com>
2019-05-12 19:24:50 -0700

    make it look pretty

--- index.js
+++ index.js
@@ -62,29 +62,7 @@ api.get(['/is/:param', '/isBase64/:param'], async (req, res) => {
 })
 
 api.get('/', (req, res) => {
-  res.send(`
-  Base 64 Microservice
-
-  Endpoints:
-    /:param    ~ if :param is a valid base 64 hash it will be decoded an returned
-                 if the decoded hash is a valid url then you will automatically be redirected to it
-                  ex: /aHR0cHM6Ly9nb29nbGUuY29t goes to https://google.com
-                 if the hash is not base 64 it will be encoded and returned
-
-    /d/:param  ~ decode :param and return it
-                 This will not redirect you if the hash is a valid url
-
-    /e/:param  ~ encode :param and return it
-
-    /is/:param ~ return true if the hash is valid base 64, else return false
-
-    /rick      ~ just try it for yourself
-    
-`)
-  //if link option to redirect
-  //link to decode and encode
-  //if no option given if valid base 64 decode
-  // else encode
+  res.sendFile('index.html', { root: __dirname })
 })
 app.use('/', api)
 app.listen(3000, () => {
